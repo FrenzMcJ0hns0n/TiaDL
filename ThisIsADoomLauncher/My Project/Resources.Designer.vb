0be78ffa1299ec02cc4ce6ec05ab52ea8bd21bb5
François MAZOYER <205885@supinfo.com>
2018-07-20 18:36:21 +0200

    Changed settings order in App.config : no change in GUI Project properties:Settings though. Deleted useless referenced Folder image in Resources.

--- ThisIsADoomLauncher/My Project/Resources.Designer.vb
+++ ThisIsADoomLauncher/My Project/Resources.Designer.vb
@@ -59,15 +59,5 @@ Namespace My.Resources
                 resourceCulture = value
             End Set
         End Property
-        
-        '''<summary>
-        '''  Recherche une ressource localisée de type System.Drawing.Bitmap.
-        '''</summary>
-        Friend ReadOnly Property Folder() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("Folder", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
     End Module
 End Namespace
