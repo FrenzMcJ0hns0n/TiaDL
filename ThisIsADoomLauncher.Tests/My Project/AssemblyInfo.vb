daf09aca89c1838864811b211bc9881465ab3660
elpedroluis <pierrelouis.mazoyer@diiage.org>
2018-08-07 14:33:08 +0200

    Added a unit tests project ;)

---
+++ ThisIsADoomLauncher.Tests/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("ThisIsADoomLauncher.Tests")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ThisIsADoomLauncher.Tests")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("6751f5c7-572a-4bd0-acb5-58fe055132ba")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ ThisIsADoomLauncher.Tests/UnitTest1.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()> Public Class DoomWorldAPITests
+    <TestMethod()> Public Async Function GetGameInfosByIdTest() As Task
+
+        Dim client As ThisIsADoomLauncher.Helpers.API.DoomWorldClient = New Helpers.API.DoomWorldClient()
+
+        Dim actual = Await client.GetGameInfosById(1)
+
+        Assert.IsNotNull(actual)
+
+    End Function
+End Class
