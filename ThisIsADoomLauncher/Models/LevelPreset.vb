185e48519efa9fa094c7abe7871c22be573bb1f0
François MAZOYER <francois.mazoyer@outlook.fr>
2022-07-20 12:07:12 +0200

    Fix Freedoom "Base" being Doom 1

--- ThisIsADoomLauncher/Models/LevelPreset.vb
+++ ThisIsADoomLauncher/Models/LevelPreset.vb
@@ -4,7 +4,7 @@
         'Preset information
         Public Property Name As String
         Public Property Desc As String 'Short description, for Tooltip text
-        Public Property Base As String 'Base assets : Doom 1 / Doom2 / Other
+        Public Property Base As String 'Base assets type : Doom 1 / Doom2 / Other
         Public Property Year As Integer 'Year of last release
 
         'Associated files
