3d2ab18c978b84f22a56ef365a7d2e358c3588ac
elpedroluis <pierrelouis.mazoyer@diiage.org>
2022-07-28 22:24:38 +0200

    Add HttpClient to request doomworld api

---
+++ ThisIsADoomLauncher/Helpers/DoomWorld/DoomWorldHttpClient.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Net.Http
+Namespace Helpers.DoomWorld
+
+    Public Class DoomWorldHttpClient
+
+        Public Const BASE_URL As String = "https://www.doomworld.com/idgames/api/"
+
+        Private Shared _instance As HttpClient
+
+        Public Shared Function GetInstance() As HttpClient
+            If _instance Is Nothing Then
+                _instance = New HttpClient()
+                _instance.BaseAddress = New Uri(BASE_URL)
+            End If
+            Return _instance
+        End Function
+
+
+    End Class
+End Namespace
---
+++ ThisIsADoomLauncher/Helpers/DoomWorld/DoomWorldService.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Net.Http
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+
+Namespace Helpers.DoomWorld
+    Public Class DoomWorldService
+
+        ''' <summary>
+        ''' Get directories
+        ''' </summary>
+        ''' <param name="parentDirectory"> Parent directory. If empty, get root levels directories</param>
+        ''' <returns></returns>
+        Public Shared Async Function GetDirectories(parentDirectory As String) As Task(Of List(Of Object))
+            Dim directories As List(Of Object) = New List(Of Object)
+
+            Dim uriPath As String = "api.php?action=getdirs&name=levels/"
+            Dim requestUri As Uri = New Uri(String.Concat(DoomWorldHttpClient.BASE_URL, uriPath, parentDirectory, "&out=json"))
+            Dim response As HttpResponseMessage = Await DoomWorldHttpClient.GetInstance().GetAsync(requestUri)
+            If response.IsSuccessStatusCode Then
+                Dim folders As List(Of Object) = New List(Of Object)
+
+                Dim jsonResult As String = Await response.Content.ReadAsStringAsync()
+
+                Dim jsonObject = JObject.Parse(jsonResult)
+
+                jsonObject.SelectToken("content.dir").ToList().ForEach(
+                    Sub(directory) directories.Add(
+                        New With {
+                        Key .id = directory.Item("id"),
+                        Key .name = directory.Item("name")}))
+
+
+            End If
+            Return directories
+
+        End Function
+
+    End Class
+End Namespace
