7e16428aa7ede8d1ac93dcc8fa775e3b34f247d9
François MAZOYER <francois.mazoyer@outlook.fr>
2022-08-14 13:48:34 +0200

    Restore function GetLevelPresets_FromCsv()

--- ThisIsADoomLauncher/Helpers/PresetsMethods.vb
+++ ThisIsADoomLauncher/Helpers/PresetsMethods.vb
@@ -54,36 +54,19 @@ Friend Module PresetsMethods
                     Try
                         Dim parsedValues As String() = parser.ReadFields()
 
-                        If presetsType = "base_levels" Then
+                        If parsedValues.Length < 4 Then Continue Do 'Skip if first 4 mandatory data are not preset: Name, Type, Year, Iwad
+                        If parsedValues(0) = "Wolfenstein 3D" Then Continue Do 'Skip Wolf3D, as not functional yet
 
-                            If parsedValues.Length < 4 Then Continue Do 'Skip if first 4 mandatory data are not preset: Name, Type, Year, Iwad
-                            If parsedValues(0) = "Wolfenstein 3D" Then Continue Do 'Skip Wolf3D, as not functional yet
-
-                            levelPresets.Add(New LevelPreset() With
-                            {
-                                .Name = parsedValues(0),
-                                .Type = parsedValues(1),
-                                .Year = Convert.ToInt32(parsedValues(2)),
-                                .Iwad = parsedValues(3),
-                                .Maps = If(parsedValues.Length >= 5, parsedValues(4), String.Empty),
-                                .Misc = If(parsedValues.Length >= 6, parsedValues(5), String.Empty),
-                                .Pict = If(parsedValues.Length = 7, parsedValues(6), String.Empty)
-                            })
-
-                        Else
-
-                            If parsedValues.Length < 3 Then Continue Do 'Skip if first 3 mandatory data are not preset: Name, Iwad, Maps
-
-                            levelPresets.Add(New LevelPreset() With
-                            {
-                                .Name = parsedValues(0),
-                                .Iwad = parsedValues(1),
-                                .Maps = parsedValues(2),
-                                .Misc = If(parsedValues.Length >= 4, parsedValues(3), String.Empty)
-                            })
-
-                        End If
-
+                        levelPresets.Add(New LevelPreset() With
+                        {
+                            .Name = parsedValues(0),
+                            .Type = parsedValues(1),
+                            .Year = Convert.ToInt32(parsedValues(2)),
+                            .Iwad = parsedValues(3),
+                            .Maps = If(parsedValues.Length >= 5, parsedValues(4), String.Empty),
+                            .Misc = If(parsedValues.Length >= 6, parsedValues(5), String.Empty),
+                            .Pict = If(parsedValues.Length = 7, parsedValues(6), String.Empty)
+                        })
 
                     Catch mleEx As MalformedLineException
                         WriteToLog($"{Date.Now} - Error : Got MalformedLineException while parsing presets {presetsType}")
