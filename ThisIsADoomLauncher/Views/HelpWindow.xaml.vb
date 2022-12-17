c426d76fcc23be8cbb4f021f7a9c1bff926336e0
François MAZOYER <francois.mazoyer@outlook.fr>
2022-12-17 11:04:03 +0100

    Finish HelpAbout

--- ThisIsADoomLauncher/Views/HelpWindow.xaml.vb
+++ ThisIsADoomLauncher/Views/HelpWindow.xaml.vb
@@ -1,5 +1,17 @@
 ﻿Namespace Views
     Public Class HelpWindow
 
+        Private Sub PM_RequestNavigate(sender As Object, e As RequestNavigateEventArgs)
+            Process.Start(New ProcessStartInfo(e.Uri.AbsoluteUri))
+        End Sub
+
+        Private Sub GZDoom_RequestNavigate(sender As Object, e As RequestNavigateEventArgs)
+            Process.Start(New ProcessStartInfo(e.Uri.AbsoluteUri))
+        End Sub
+
+        Private Sub ModDB_RequestNavigate(sender As Object, e As RequestNavigateEventArgs)
+            Process.Start(New ProcessStartInfo(e.Uri.AbsoluteUri))
+        End Sub
+
     End Class
 End Namespace
