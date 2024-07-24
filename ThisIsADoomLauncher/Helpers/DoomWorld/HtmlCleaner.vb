d38c8cc4dfd5be6eacde9200844c05d09ec58f89
Frenzowski <francois.christophe.mazoyer@gmail.com>
2024-07-25 00:11:43 +0200

    Minor edits

--- ThisIsADoomLauncher/Helpers/DoomWorld/HtmlCleaner.vb
+++ ThisIsADoomLauncher/Helpers/DoomWorld/HtmlCleaner.vb
@@ -1,6 +1,4 @@
-﻿Imports System.Drawing.Imaging
-Imports System.IO
-Imports System.Reflection
+﻿Imports System.Reflection
 Imports System.Text.RegularExpressions
 
 Namespace Helpers.DoomWorld
@@ -12,16 +10,16 @@ Namespace Helpers.DoomWorld
         ''' <param name="html"></param>
         ''' <returns></returns>
         Public Shared Function HtmlToPlainText(html As String) As String
+            Dim text As String = html
 
             Const tagWhiteSpace As String = "(>|$)(\W|\n|\r)+<" 'matches one Or more (white space Or line breaks) between '>' and '<'
             Const stripFormatting As String = "<[^>]*(>|$)" 'match any character between '<' and '>', even when end tag is missing
             Const lineBreak As String = "<(br|BR)\s{0,1}\/{0,1}>\s{0,1}" 'matches: <br>,<br/>,<br />,<BR>,<BR/>,<BR />,"<br> ","<BR> "
 
-            Dim lineBreakRegex As Regex = New Regex(lineBreak, RegexOptions.Multiline)
-            Dim stripFormattingRegex As Regex = New Regex(stripFormatting, RegexOptions.Multiline)
-            Dim tagWhiteSpaceRegex As Regex = New Regex(tagWhiteSpace, RegexOptions.Multiline)
+            Dim lineBreakRegex As New Regex(lineBreak, RegexOptions.Multiline)
+            Dim stripFormattingRegex As New Regex(stripFormatting, RegexOptions.Multiline)
+            Dim tagWhiteSpaceRegex As New Regex(tagWhiteSpace, RegexOptions.Multiline)
 
-            Dim text As String = html
             Try
                 'Decode html specific characters
                 text = System.Net.WebUtility.HtmlDecode(text)
@@ -35,9 +33,7 @@ Namespace Helpers.DoomWorld
             Catch ex As Exception
                 Dim currentMethodName As String = MethodBase.GetCurrentMethod().Name
                 WriteToLog($"{Date.Now} - Error in '{currentMethodName}'{vbCrLf} Exception : {ex}{vbCrLf} Parameter(s) : {html}")
-
             End Try
-
 
             Return text
         End Function
